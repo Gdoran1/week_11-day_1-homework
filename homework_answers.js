8daf5d968db15d2a950bbf508ae29b01383fa496
graeme doran <graeme42@icloud.com>
2017-12-04 17:58:57 +0000

    homework answers

---
+++ homework_answers.js
@@ -0,0 +1,13 @@
+// 1. my name is keith
+
+// 2. 3 as thats inside the function and the 5 is global
+
+// 3. 0: Ducks, 1: Dogs, 2: Lions
+//    (chickens, cats, rabbits outside of function)
+
+// 4. suspects include: jay, val,harvey & rick.
+//    suspect 3 is keith
+
+// 5. poirot calling decectiveInfo Function
+
+// 6.the murderer is rick
